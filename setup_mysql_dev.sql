10891df4c8d72dc63d63c0ec5fc32552f667b56f
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-18 01:11:35 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,10 @@
--- setting up a database as stated in task #3
+-- Setting up a database as stated in task #3
+-- Database creation
 create database if not exists hbnb_dev_db;
+-- User creation
 create user if not exists "hbnb_dev"@"localhost" identified by "hbnb_dev_pwd";
+-- privileges granting
 grant all privileges on hbnb_dev_db.* to "hbnb_dev"@"localhost";
+-- privileges granting
 grant select on performance_schema.* to "hbnb_dev"@"localhost";
 flush privileges;
