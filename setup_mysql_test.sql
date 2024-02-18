5992fe9586ac2f7552a033a6d6f00da8f87d388b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-18 15:25:32 +0200

    4. MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
--- Setting up a database as stated in task #3
+-- Setting up a database as stated in task #4
 -- Database creation
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- User creation
-CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
+CREATE USER IF NOT EXISTS "hbnb_test"@"localhost" IDENTIFIED BY "hbnb_test_pwd";
 -- privileges granting
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO "hbnb_test"@"localhost";
 FLUSH PRIVILEGES;
 -- privileges granting
-GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
+GRANT SELECT ON performance_schema.* TO "hbnb_test"@"localhost";
 FLUSH PRIVILEGES;
