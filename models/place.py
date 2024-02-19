2707456d1dc2ba96735017025c696454634ae1db
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-19 12:11:54 +0200

    8. DBStorage - Place

--- models/place.py
+++ models/place.py
@@ -16,13 +16,13 @@ class Place(BaseModel, Base):
         city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
         user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
         name = Column(String(128), nullable=Fales)
-        description = Column(String(1024), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=False)
-        longitude = Column(Float, nullable=False)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
     else:
         city_id = ""
         user_id = ""
