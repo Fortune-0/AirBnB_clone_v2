43fc858e76e128358d0c21054f3a0c7bc2939613
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-21 13:14:30 +0200

    Improve

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models import Base, BaseModel
+from models import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
