4638f0c5cab8ca90cff88b290dbd8b5f1611f3f5
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-06 17:09:55 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 # Sets up your web servers for the deployment of web_static
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "Simple content, to test the configuration" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data/
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "Simple content, to test the configuration" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu:ubuntu /data/
 file='/etc/nginx/sites-available/default'
 line=56
 text='\t}\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}'
-sudo sed -i "${line}s~.*~${text}~" $file
-sudo service nginx restart
+sed -i "${line}s~.*~${text}~" $file
+service nginx restart
