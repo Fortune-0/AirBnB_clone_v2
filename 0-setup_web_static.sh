7be7f33e480ba863c46a0427c2f0d0bebc426e1b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-06 17:14:36 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,6 +4,7 @@ apt-get -y update
 apt-get -y install nginx
 service nginx start
 mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 touch /data/web_static/releases/test/index.html
 echo "Simple content, to test the configuration" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 ln -sf /data/web_static/releases/test/ /data/web_static/current
