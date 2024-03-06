0be6aded18ae9efc4eda30e9d35a849d15a2953d
MegaChie <workspace.mega@gmail.com>
2024-03-06 11:24:51 -0800

    2. Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1. Compress before sending"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static"""
+    # getting time and formating it
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # naming the file
+    name = "web_static_{}.tgz".format(time)
+
+    # command
+    comm = "tar -cvzf versions/{} web_static/".format(name)
+
+    # execution
+    local('mkdir -p versions/')
+    path = local(comm)
+    if path.succeeded:
+        return "versions/{}".format(name)
