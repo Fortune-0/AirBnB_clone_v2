2da482515dce26c9b4eed304db878d79d73669db
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-07 16:05:31 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,24 +16,24 @@ def do_deploy(archive_path):
         # placing the archive
         put(archive_path, "/tmp/")
 
-        # # getting name of archive from archive_path
-        # temp = str(archive_path).split("/")[-1]
-        # name = temp.split(".")[0]
+        # getting name of archive from archive_path
+        temp = str(archive_path).split("/")[-1]
+        name = temp.split(".")[0]
 
-        # # uncompressing...
-        # # extraction path
-        # extrPath = "/data/web_static/releases/{}/".format(name)
-        # run("mkdir -p {}".format(extrPath))
-        # run("tar -xzf /tmp/{} -C {}".format(temp, extrPath))
-        # run("mv {}/web_static/* {}".format(extrPath, extrPath))
-        # # removing extracted
-        # run("rm /tmp/{}".format(temp))
+        # uncompressing...
+        # extraction path
+        extrPath = "/data/web_static/releases/{}/".format(name)
+        run("mkdir -p {}".format(extrPath))
+        run("tar -xzf /tmp/{} -C {}".format(temp, extrPath))
+        run("mv {}/web_static/* {}".format(extrPath, extrPath))
+        # removing extracted
+        run("rm /tmp/{}".format(temp))
 
-        # # deletes the symbolic
-        # run("rm -rf /data/web_static/current")
+        # deletes the symbolic
+        run("rm -rf /data/web_static/current")
 
-        # # new symbolic link
-        # run("ln -s {} /data/web_static/current".format(extrPath))
+        # new symbolic link
+        run("ln -s {} /data/web_static/current".format(extrPath))
         return True
     except Exception:
         return False
