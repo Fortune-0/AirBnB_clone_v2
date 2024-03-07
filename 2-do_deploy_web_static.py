711339dd8d5dd4070d501911fd61b7a755d357e4
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-07 15:10:14 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,9 +14,9 @@ def do_deploy(archive_path):
         # placing the archive
         put(archive_path, "/tmp/")
 
-        # getting name of archive from archive_path
-        temp = str(archive_path).split("/")[-1]
-        name = temp.split(".")[0]
+        # # getting name of archive from archive_path
+        # temp = str(archive_path).split("/")[-1]
+        # name = temp.split(".")[0]
 
         # uncompressing...
         # extraction path
@@ -27,11 +27,11 @@ def do_deploy(archive_path):
         # removing extracted
         run("rm /tmp/{}".format(temp))
 
-        # deletes the symbolic
-        run("rm -rf /data/web_static/current")
+        # # deletes the symbolic
+        # run("rm -rf /data/web_static/current")
 
-        # new symbolic link
-        run("ln -s {} /data/web_static/current".format(extrPath))
-        return True
+        # # new symbolic link
+        # run("ln -s {} /data/web_static/current".format(extrPath))
+        # return True
     except Exception:
         return False
