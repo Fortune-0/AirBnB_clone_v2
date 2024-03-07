a8df37ceb871bb2311f631e0f7b222d956d053a8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-07 14:35:05 +0200

    5. Puppet for setup

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,62 @@
+# Redos task #0 but by using Puppet
+
+exec {'updateing the packages':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['installing Nginx'],
+}
+
+exec {'installing Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['starting Nginx'],
+}
+
+exec {'starting Nginx':
+  provider => shell,
+  command  => 'sudo service nginx start',
+  before   => Exec['creating directory'],
+}
+
+exec {'creating directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/releases/test/',
+  before   => Exec['creating another directory'],
+}
+
+exec {'creating another directory':
+  provider => shell,
+  command  => 'sudo mkdir -p /data/web_static/shared/',
+  before   => Exec['html'],
+}
+
+exec {'html':
+  provider => shell,
+  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
+  before   => Exec['symbolic link'],
+}
+
+exec {'symbolic link':
+  provider => shell,
+  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  before   => Exec['location'],
+}
+
+exec {'location':
+  provider => shell,
+  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
+  before   => Exec['restart Nginx'],
+}
+
+exec {'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+  before   => File['/data/']
+}
+
+file {'/data/':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+}
