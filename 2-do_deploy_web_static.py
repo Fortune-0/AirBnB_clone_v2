d8c5538668ede7cafbf165460057a38c829d5085
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-08 01:14:38 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -38,5 +38,6 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(extrPath))
         print("New version deployed!")
         return True
-    except Exception:
+    except Exception as nope:
+        print(nope)
         return False
