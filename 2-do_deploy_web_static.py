fa221c1815f98425ba31b5d40dc225ce58663b23
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-08 22:11:26 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,8 @@
 from fabric.api import put, run, env
 from datetime import datetime
 import os
+from fabric import Connection
+import pysftp as sftp
 
 
 env.hosts = ["54.226.42.171", "54.236.16.117"]
@@ -15,29 +17,31 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
     try:
-        # getting name of archive from archive_path
-        temp = str(archive_path).split("/")[-1]
-        name = temp.split(".")[0]
+        for link in env.hosts:
+            with sftp.Connection(host= link, username = env.user):
+                # getting name of archive from archive_path
+                temp = str(archive_path).split("/")[-1]
+                name = temp.split(".")[0]
 
-        # placing the archive
-        put(archive_path, "/tmp/")
+                # placing the archive
+                put(archive_path, "/tmp/")
 
-        # uncompressing...
-        # extraction path
-        extrPath = "/data/web_static/releases/{}/".format(name)
-        run("mkdir -p {}".format(extrPath))
-        run("tar -xzf /tmp/{} -C {}".format(temp, extrPath))
-        run("mv {}/web_static/* {}".format(extrPath, extrPath))
-        # removing extracted
-        run("rm /tmp/{}".format(temp))
+                # uncompressing...
+                # extraction path
+                extrPath = "/data/web_static/releases/{}/".format(name)
+                run("mkdir -p {}".format(extrPath))
+                run("tar -xzf /tmp/{} -C {}".format(temp, extrPath))
+                run("mv {}/web_static/* {}".format(extrPath, extrPath))
+                # removing extracted
+                run("rm /tmp/{}".format(temp))
 
-        # deletes the symbolic
-        run("rm -rf /data/web_static/current")
+                # deletes the symbolic
+                run("rm -rf /data/web_static/current")
 
-        # new symbolic link
-        run("ln -s {} /data/web_static/current".format(extrPath))
-        print("New version deployed!")
-        return True
+                # new symbolic link
+                run("ln -s {} /data/web_static/current".format(extrPath))
+                print("New version deployed!")
+                return True
     except Exception as nope:
         print(nope)
         return False
