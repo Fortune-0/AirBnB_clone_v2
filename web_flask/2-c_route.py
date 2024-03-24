dee92b847e7922c3c6e90ac7285ebc3ea884edc0
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-25 00:48:57 +0100

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,23 @@
-#!/usr/env/python3
-"""A script that starts a flask web application
+#!/usr/bin/python3
+""" Write a script that starts a Flask web application
 """
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """returns a given string"""
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
     return ("Hello HBNB!")
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """returns a given string"""
+    """Returns a given string"""
     return ("HBNB")
 
-# @app.route("/c/<text>", strict_slashes=False)
-# def hbnb_c(text):
-#     """returns a modified string bassed on the input"""
-#     # if text == "is_fun":
-#     #     return "C is fun"
-    
-#     return (" C {}".formart (text.replace("_", " ")))
 
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
@@ -30,5 +25,12 @@ def cText(text):
     return "C {}".format(text.replace("_", " "))
 
 
+# @app.route('/python', strict_slashes=False)
+# @app.route("/python/<text>", strict_slashes=False)
+# def pythonText(text="is cool"):
+#     """display Python followed by the value of the text variable"""
+#     return "Python {}".format(text.replace("_", " "))
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=None)
