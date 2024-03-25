4ba0f92cc12de9926ebd7cd2ec43ae49a7315edd
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-25 01:44:26 +0100

    update

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
+""" Write a script that starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask("__name__")
 
 
@@ -11,15 +12,30 @@ def hello():
     """Return a given string"""
     return ("Hello HBNB!")
 
-@app.route('/hbnb', strict_slashes=False)
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Returns the supplied string"""
+    """Returns a given string"""
     return ("HBNB")
 
-@app.route('/c/<text>', strict_slashes=False)
-def C_text(text):
-    """Returns the supplied argument as a"""
-    return ("C {}".format(text.replace('_', ' ')))
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonText(text):
+    """display Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route("/number/<num>", strict_slashes=False)
+def  number(num):
+    """Display only an integer"""
+    return ("{} " .format(int(num)) + "is a number")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
