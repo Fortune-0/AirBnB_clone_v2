54ab35611af1a8ecc776c955341fe5b1187c3ba2
Fortune-0 <fortunepeterspc07@gmail.com>
2024-03-25 01:09:31 +0100

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,21 @@
-#!/usr/env/python3
+#!/usr/bin/python3
 """A script that starts a flask web application
 """
 
 from flask import Flask
+app = Flask("__name__")
 
-app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """returns a given string"""
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
     return ("Hello HBNB!")
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns a given string"""
+    """Returns the supplied string"""
     return ("HBNB")
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=None)
